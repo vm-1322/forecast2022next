cb4e91dc3d0b109df41aa28b09a7adf5790e977d
Volodymyr <vm132002@gmail.com>
2022-11-25 18:54:49 +0200

    Fixed bug Log Out

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -57,8 +57,8 @@ const Navigation: React.FC<INavigationProps> = ({
               <Link
                 href={'/'}
                 onClick={() => {
-                  router.replace('/');
                   signOut();
+                  router.replace('/');
                 }}
               >
                 Log Out
